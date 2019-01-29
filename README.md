7c38d7f58ee67c1af175afb6a15619d1bc8f4457
knee-cola <nikola.derezic@gmail.com>
2019-01-30 00:16:20 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# What't this?
+This is a basic websocket example made based on the example found at https://medium.com/@martin.sikora/node-js-websocket-simple-chat-tutorial-2def3a841b61
+
+# How to run it:
+Open two bash terminals. In the first one run `node ws-server.js`, while in the second run `node static-file-server.js`. In your browser visit address http://localhost:8080.
---
+++ client.js
@@ -0,0 +1,44 @@
+// https://medium.com/@martin.sikora/node-js-websocket-simple-chat-tutorial-2def3a841b61
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let wsServer = initServer();
+
+    const tbxMsg = document.getElementById("tbxMsg");
+    const cmdSend = document.getElementById("cmdSend");
+
+    cmdSend.addEventListener('click', () => {
+        wsServer.send(tbxMsg.value);
+    });
+
+});
+
+
+const initServer = () => {
+    
+    const wsServer = new WebSocket('ws://localhost:1337');
+
+    wsServer.onopen = function () {
+        // connection is opened and ready to use
+        console.log('on open');
+    };
+
+    wsServer.onerror = function (error) {
+        // an error occurred when sending/receiving data
+        console.log('on error');
+    };
+    
+    wsServer.onmessage = function (message) {
+        console.log('on message');
+
+        console.dir(message);
+ //       try {
+ //           var json = JSON.parse(message.data);
+ //       } catch (e) {
+ //           console.log('This doesn\'t look like a valid JSON: ', message.data);
+ //           return;
+ //       }
+    };
+
+    return(wsServer);
+}
